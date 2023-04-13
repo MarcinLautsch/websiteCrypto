94e5863f0a5b8c333005441e0327a1aca41cf8c5
Marcin Lautsch <mldemigod@gmail.com>
2023-04-13 17:15:08 +0200

    headerFinish

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const navMobile = document.querySelector('.nav-mobile')
+const navBtm = document.querySelector('.hamburger')
+const footerYear = document.querySelector('.footer__year')
+
+const handleNav = () => {
+	navBtm.classList.toggle('is-active')
+	navMobile.classList.toggle('nav-mobile--active')
+}
+
+navBtm.addEventListener('click', handleNav)
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
+}
+
+handleCurrentYear()
