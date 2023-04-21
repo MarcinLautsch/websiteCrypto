46794ea549d3c5bbf413df707a7e06ca22c301a3
Marcin Lautsch <mldemigod@gmail.com>
2023-04-21 11:25:49 +0200

    cryptoFinish

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
 const navMobile = document.querySelector('.nav-mobile')
+const body = document.querySelector('body')
 const navBtm = document.querySelector('.hamburger')
 const footerYear = document.querySelector('.footer__year')
 
 const handleNav = () => {
 	navBtm.classList.toggle('is-active')
 	navMobile.classList.toggle('nav-mobile--active')
+	body.classList.toggle('scroll-lock')
 }
+
+const activateScroll = () => {
+	navBtm.classList.remove('is-active')
+	navMobile.classList.remove('nav-mobile--active')
+	body.classList.remove('scroll-lock')
+}
+
+const links = document.querySelectorAll('.nav__link')
+links.forEach(link => {
+	link.addEventListener('click', activateScroll)
+})
 
 navBtm.addEventListener('click', handleNav)
 
